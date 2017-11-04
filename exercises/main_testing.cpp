b721047284865de652eb44200b51f3dd24df8066
Maciej Patro <maciej.patro@gmail.com>
2017-11-04 21:44:17 +0100

    Catch v2 test failing

---
+++ exercises/main_testing.cpp
@@ -0,0 +1,14 @@
+/***********************************************
+*
+* Created by mpatro on 11/4/17.
+*
+***********************************************/
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+int theAnswer() { return 6*9; }
+
+TEST_CASE( "Life, the universe and everything", "[42][theAnswer]" ) {
+    REQUIRE( theAnswer() == 42 );
+}
