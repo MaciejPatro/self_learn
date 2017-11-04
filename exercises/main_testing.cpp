f43a308f59a542979ff3094d2dd38ed4e92bba7b
Maciej Patro <maciej.patro@gmail.com>
2017-11-04 21:49:43 +0100

    CLion setup to use catch tests + clang_format the file

--- exercises/main_testing.cpp
+++ exercises/main_testing.cpp
@@ -1,14 +1,12 @@
 /***********************************************
-*
-* Created by mpatro on 11/4/17.
-*
-***********************************************/
+ *
+ * Created by mpatro on 11/4/17.
+ *
+ ***********************************************/
 
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
-int theAnswer() { return 6*9; }
+int theAnswer() { return 42; }
 
-TEST_CASE( "Life, the universe and everything", "[42][theAnswer]" ) {
-    REQUIRE( theAnswer() == 42 );
-}
+TEST_CASE("Life, the universe and everything", "[42][theAnswer]") { REQUIRE(theAnswer() == 42); }
