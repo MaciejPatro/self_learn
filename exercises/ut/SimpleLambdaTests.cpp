84d079c9df725f1e704307a71fc2b17a42ca4f08
Maciej Patro <maciej.patro@gmail.com>
2017-11-05 17:57:29 +0100

    [ongoing][lambdas] - greater than for several type. {TODO} fix clang format - it's bad

--- exercises/ut/SimpleLambdaTests.cpp
+++ exercises/ut/SimpleLambdaTests.cpp
@@ -13,4 +13,26 @@ TEST_CASE("Having fun with lambdas", "[lambdas]")
     auto always_return_true = []() { return true; };
     REQUIRE(always_return_true());
   }
+  SECTION("Lambda checking whether LHS > RHS")
+  {
+    class MyType
+    {
+       public:
+      MyType(int i)
+          : someVar(i)
+      {
+      }
+      bool operator>(const MyType& arg) const noexcept { return this->someVar > arg.someVar; }
+
+       private:
+      int someVar;
+    };
+
+    MyType four{4};
+    MyType two{2};
+    auto   greater_than_lambda = [](auto lhs, auto rhs) { return lhs > rhs; };
+    REQUIRE(greater_than_lambda(5, 4));
+    REQUIRE(greater_than_lambda(5.23, 4.3));
+    REQUIRE(greater_than_lambda(four, two));
+  }
 }
