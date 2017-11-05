a1d5252a850e29f5a1d61b6db3080891722f3a33
Maciej Patro <maciej.patro@gmail.com>
2017-11-05 18:12:09 +0100

    Revert "failing jenkins"
    This reverts commit 6ef963c7e8468112ba9857cd61efb4ebc6c6ec32.

--- exercises/ut/SimpleLambdaTests.cpp
+++ exercises/ut/SimpleLambdaTests.cpp
@@ -11,15 +11,28 @@ TEST_CASE("Having fun with lambdas", "[lambdas]")
   SECTION("Always return true lambda")
   {
     auto always_return_true = []() { return true; };
-
     REQUIRE(always_return_true());
   }
-
-  SECTION("Lambda checking whether lhs > rhs")
+  SECTION("Lambda checking whether LHS > RHS")
   {
-    auto greater_than_lambda = [](auto lhs, auto rhs) { return lhs > rhs; };
+    class MyType
+    {
+       public:
+      MyType(int i)
+          : someVar(i)
+      {
+      }
+      bool operator>(const MyType& arg) const noexcept { return this->someVar > arg.someVar; }
 
-    REQUIRE(greater_than_lambda(4, 4));
+       private:
+      int someVar;
+    };
+
+    MyType four{4};
+    MyType two{2};
+    auto   greater_than_lambda = [](auto lhs, auto rhs) { return lhs > rhs; };
+    REQUIRE(greater_than_lambda(5, 4));
     REQUIRE(greater_than_lambda(5.23, 4.3));
+    REQUIRE(greater_than_lambda(four, two));
   }
 }
