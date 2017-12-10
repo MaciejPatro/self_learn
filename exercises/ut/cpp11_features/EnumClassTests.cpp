657b81af813a0e8c6db1ee8689a571be0ade6704
Maciej Patro <maciej.patro@gmail.com>
2017-12-10 13:21:37 +0100

    CPP11 - enum class benefits checked

--- exercises/ut/cpp11_features/EnumClassTests.cpp
+++ exercises/ut/cpp11_features/EnumClassTests.cpp
@@ -4,3 +4,50 @@
 *
 ***********************************************/
 
+#include <catch.hpp>
+
+/*
+   C++11 introduces a new null pointer type designed to replace C's NULL macro. nullptr itself is of type
+ std::nullptr_t and can be implicitly converted into pointer types, and unlike NULL, not convertible to integral types
+ except bool.
+*/
+
+namespace {
+
+//forward declaration
+enum class Color : bool;
+bool using_color(Color)
+{
+  return true;
+}
+
+enum class Color : bool
+{
+  Black,
+  White,
+};
+
+} // namespace
+
+TEST_CASE("[CPP11] enum class", "[cpp11][enum_class]")
+{
+  SECTION("benefit")
+  {
+    auto color = Color::Black;
+    SECTION("is strongly typed")
+    {
+      REQUIRE(color != Color::White);
+      // REQUIRE(0 != Color::Black); not compiling - Black is class
+    }
+
+    SECTION("from having well-defined size")
+    {
+      REQUIRE(sizeof(bool) == sizeof(Color));
+    }
+
+    SECTION("from having possible forward declarations")
+    {
+      REQUIRE(using_color(Color::Black));
+    }
+  }
+}
