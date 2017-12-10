b97a401d52a6faee5eb886a038cee1d51090f0a5
Maciej Patro <maciej.patro@gmail.com>
2017-12-10 11:09:22 +0100

    CPP11 - nullptr tests introduced

--- exercises/ut/cpp11_features/NullPtrTests.cpp
+++ exercises/ut/cpp11_features/NullPtrTests.cpp
@@ -5,6 +5,7 @@
 ***********************************************/
 
 #include <catch.hpp>
+#include <memory>
 
 /*
    C++11 introduces a new null pointer type designed to replace C's NULL macro. nullptr itself is of type
@@ -13,8 +14,36 @@
 */
 
 namespace {
+
+bool fnc(int)
+{
+  return false;
+};
+
+bool fnc(char*)
+{
+  return true;
+};
+
+bool expecting_unique_ptr_function(std::unique_ptr<int>)
+{
+  return true;
+}
+
 } // namespace
 
 TEST_CASE("[CPP11] nullptr usage", "[cpp11][nullptr]")
 {
+  SECTION("Specific function can be called only with nullptr or int. NULL is ambiguous and is not compiling")
+  {
+    // fnc(NULL); is ambiguous!
+    REQUIRE(fnc(nullptr));
+    REQUIRE_FALSE(fnc(5));
+  }
+
+  SECTION("Nullptr can be used with unique/shared pointers")
+  {
+    REQUIRE(expecting_unique_ptr_function(nullptr));
+    // REQUIRE(expecting_unique_ptr_function(NULL)); incorrect type - will not compile
+  }
 }
